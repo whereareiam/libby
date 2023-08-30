e9e7be8b80459f476d86abdc32cc7c32b060a0c4
bivashy <botyrbojey@gmail.com>
2023-08-30 13:09:01 +0600

    Return Collection<Library> in TransitiveLibraryManager#loadLibraryTransitively

--- transitive/src/main/java/net/byteflux/libby/TransitiveLibraryManager.java
+++ transitive/src/main/java/net/byteflux/libby/TransitiveLibraryManager.java
@@ -1,18 +1,19 @@
 package net.byteflux.libby;
 
-import net.byteflux.libby.helper.DependencyTreeHelper;
-import net.byteflux.libby.logging.adapters.LogAdapter;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactDescriptorException;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.stream.Stream;
-
-import static net.byteflux.libby.helper.DependencyTreeHelper.findCompileDependencies;
+import net.byteflux.libby.helper.DependencyTreeHelper;
+import net.byteflux.libby.logging.adapters.LogAdapter;
 
 public class TransitiveLibraryManager extends LibraryManager {
     private final LibraryManager delegate;
@@ -29,25 +30,27 @@ public class TransitiveLibraryManager extends LibraryManager {
         return new TransitiveLibraryManager(logAdapter, dataDirectory, directoryName, delegate);
     }
 
-    public void loadLibraryTransitively(Library library, ExcludedLibrary... excludedLibraries) {
+    public Collection<Library> loadLibraryTransitively(Library library, ExcludedLibrary... excludedLibraries) {
         loadLibrary(library);
 
         Collection<ExcludedLibrary> excludedLibraryList = Arrays.asList(excludedLibraries);
         RemoteRepository[] repositories = Stream.of(getRepositories(), library.getRepositories())
-                                                .flatMap(Collection::stream)
-                                                .map(DependencyTreeHelper::newDefaultRepository)
-                                                .toArray(RemoteRepository[]::new);
+                .flatMap(Collection::stream)
+                .map(DependencyTreeHelper::newDefaultRepository)
+                .toArray(RemoteRepository[]::new);
 
         try {
-            findCompileDependencies(library.getGroupId(), library.getArtifactId(), library.getVersion(), repositories).forEach(transitiveArtifact -> {
-                Library transitiveLibrary = adaptArtifact(library, transitiveArtifact);
-                if (excludedLibraryList.stream().noneMatch(excludedLibrary -> excludedLibrary.similar(transitiveLibrary)))
-                    loadLibrary(transitiveLibrary);
-            });
+            return DependencyTreeHelper.findCompileDependencies(library.getGroupId(), library.getArtifactId(), library.getVersion(), repositories).stream()
+                    .map(artifact -> adaptArtifact(library, artifact))
+                    .peek(transitiveLibrary -> {
+                        if (excludedLibraryList.stream().noneMatch(excludedLibrary -> excludedLibrary.similar(transitiveLibrary)))
+                            loadLibrary(transitiveLibrary);
+                    }).collect(Collectors.toList());
         } catch (DependencyResolutionException | ArtifactDescriptorException e) {
             logger.error("Cannot resolve transitive dependencies of library. Details: " + library, e);
             e.printStackTrace();
         }
+        return Collections.emptyList();
     }
 
     private Library adaptArtifact(Library parent, Artifact artifact) {
