8f96243816dd195e5aa03bad0051d28ae4ab627c
AlessioDP <alessiodptv@gmail.com>
2023-09-17 18:35:44 +0200

    Added missing junit platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ subprojects {
     dependencies {
         testImplementation(platform("org.junit:junit-bom:5.10.0"))
         testImplementation("org.junit.jupiter:junit-jupiter")
+        testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
 
     java {
