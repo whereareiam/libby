3996f93815cc6b3628f8cdba13b74908cdff535b
AlessioDP <alessiodptv@gmail.com>
2023-09-17 18:30:35 +0200

    Set source and target java compatibility to 17 in paper

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -5,5 +5,10 @@ repositories {
 dependencies {
     api(project(":libby-core"))
 
-    implementation("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT") // Higher version requires Java 17
+    implementation("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
