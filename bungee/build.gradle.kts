54654bc91635b9b0eb7f66c5631f8a6a729279c5
AlessioDP <alessiodptv@gmail.com>
2023-10-26 03:11:21 +0200

    Updated bungeecord-api version and repository

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -1,9 +1,9 @@
 repositories {
-    maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
 
 dependencies {
     api(project(":libby-core"))
 
-    implementation("net.md-5:bungeecord-api:1.20-R0.1-SNAPSHOT")
+    implementation("net.md-5:bungeecord-api:1.20-R0.2-SNAPSHOT")
 }
